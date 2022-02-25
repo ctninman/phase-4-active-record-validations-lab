c6827b20f7082fe0e83ec69796ab071b62dadd24
ctninman <89664157+ctninman@users.noreply.github.com>
2022-02-25 11:49:11 -0500

    commit tree cf9923bdfb20488ba1fd6b1697571ad9a782cf20

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+
+  BAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  
+  validate :clickbaity
+
+
+
+  # def clickbaity
+  #   if title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess" == false
+  #     errors.add(:title, "not clickbait-y enough")
+  #   end
+  # end
+
+  def clickbaity
+    if BAIT_PATTERNS.none? { |pattern| pattern.match(title) }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
